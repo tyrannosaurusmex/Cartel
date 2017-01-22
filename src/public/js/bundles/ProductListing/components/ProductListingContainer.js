fd8a8a02df99ee75de377de13d58d2da0a6a7bbf
Chris Daniells <chrisdaniells@hotmail.co.uk>
2017-01-22 21:31:08 +0000

    Working example of store state@

--- src/public/js/bundles/ProductListing/components/ProductListingContainer.js
+++ src/public/js/bundles/ProductListing/components/ProductListingContainer.js
@@ -7,7 +7,7 @@ import * as ProductListingActions from '../actions/ProductListingActions';
 @connect((store) => {
     return {
         productListing: store.productListing.products,
-        selectedCurrency: store.userOptions.currency
+        selectedCurrency: store.userOptions.options.currency
     }
 })
 
@@ -16,20 +16,13 @@ export default class ProductListingContainer extends React.Component {
         super();
         this.props = data;
 
-        this.prodList = [];
         this.props.dispatch(ProductListingActions.fetchProductListings());
-
-        for (let prod in this.props.productListing) {
-            if (this.props.productListing.hasOwnProperty(prod)) {
-                this.prodList.push(<div>{this.props.productListing[prod].artist_title}</div>)
-            }
-        }
     }
     render() {
-        let prods = this.props.productListing;
-        for (let prod in prods) {
-            if (prods.hasOwnProperty(prod)) {
-                this.prodList.push(<div>{prods[prod].artist_title}</div>)
+        this.prodList = [];
+        for (let prod in this.props.productListing) {
+            if (this.props.productListing.hasOwnProperty(prod)) {
+                this.prodList.push(<div>{this.props.productListing[prod].price[this.props.selectedCurrency]}</div>)
             }
         }
 
