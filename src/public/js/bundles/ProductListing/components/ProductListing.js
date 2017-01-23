fd2c8e9516979a7f1e97c4b11bea3e4abd8d94ee
Chris Daniells <chrisdaniells@hotmail.co.uk>
2017-01-23 10:41:13 +0000

    shit more

--- src/public/js/bundles/ProductListing/components/ProductListing.js
+++ src/public/js/bundles/ProductListing/components/ProductListing.js
@@ -7,10 +7,10 @@ export default class ProductListing extends React.Component {
     constructor(data) {
         super(data);
         this.props = data;
-    }
-    render() {
         this.prodList = [];
-
+    }
+    generateProductPanels() {
+        this.prodList = [];
         for (let prod in this.props.productListing) {
             let product = this.props.productListing[prod];
             if (this.props.productListing.hasOwnProperty(prod)) {
@@ -18,7 +18,10 @@ export default class ProductListing extends React.Component {
                 this.prodList.push(<div>{productHandler.getSymbolByCurrency(this.props.selectedCurrency)}{product.price[this.props.selectedCurrency]}</div>)
             }
         }
-        
+    }
+    render() {
+        this.generateProductPanels();
+
         return <div>{this.prodList}</div>
     }
 }
