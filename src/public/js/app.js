394c5e9bab7c0f68b5da7528cb98e86680a28f35
Chris Daniells <chrisdaniells@hotmail.co.uk>
2017-01-23 19:06:03 +0000

    React Router

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
 import { Provider } from 'react-redux';
+import { syncHistoryWithStore } from 'react-router-redux'
 import store from './store';
 
 import Config from './Config';
@@ -9,7 +11,7 @@ import * as ClassToggler from './modules/ClassToggler';
 
 import CurrencySelector from './bundles/UserOptions/components/CurrencySelector';
 import LanguageSelector from './bundles/UserOptions/components/LanguageSelector';
-import ProductListing from './bundles/ProductListing/components/ProductListingContainer';
+import ProductListingContainer from './bundles/ProductListing/components/ProductListingContainer';
 
 const currencySelectorDOM = document.getElementById('CurrencySelector');
 ReactDOM.render(<Provider store={store}>
@@ -21,7 +23,12 @@ ReactDOM.render(<Provider store={store}>
     <LanguageSelector />
 </Provider>, languageSelectorDOM);
 
+const history = syncHistoryWithStore(browserHistory, store)
 const rootDOM = document.getElementById('app');
-ReactDOM.render(<Provider store={store}>
-    <ProductListing />
-</Provider>, rootDOM);
+ReactDOM.render((
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/format/vinyl" component={ProductListingContainer} />
+        </Router>
+    </Provider>
+), rootDOM);
--- src/public/js/reducerRoot.js
+++ src/public/js/reducerRoot.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux'
 
 import userOptions from './bundles/UserOptions/reducers/userOptionsReducer';
 import productListing from './bundles/ProductListing/reducers/ProductListingReducer';
 
 export default combineReducers({
-    userOptions, productListing
+    userOptions, 
+    productListing,
+    routing: routerReducer
 });
