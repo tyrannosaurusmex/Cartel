35186a6c8234c908e92b3272ed4aa8ca3af20f93
Chris Daniells <chrisdaniells@hotmail.co.uk>
2017-01-24 18:30:51 +0000

    Tweak product listing

--- src/public/js/bundles/ProductListing/components/ProductListing.js
+++ src/public/js/bundles/ProductListing/components/ProductListing.js
@@ -7,21 +7,20 @@ export default class ProductListing extends React.Component {
     constructor(data) {
         super(data);
         this.props = data;
-        this.prodList = [];
     }
     generateProductPanels() {
-        this.prodList = [];
+        let prodList = [];
         for (let prod in this.props.productListing) {
             let product = this.props.productListing[prod];
             if (this.props.productListing.hasOwnProperty(prod)) {
                 let productHandler = new ProductHandler(product);
-                this.prodList.push(<div>{productHandler.getSymbolByCurrency(this.props.selectedCurrency)}{product.price[this.props.selectedCurrency]}</div>)
+                prodList.push(<div>{productHandler.getSymbolByCurrency(this.props.selectedCurrency)}{product.price[this.props.selectedCurrency]}</div>)
             }
         }
+
+        return prodList;
     }
     render() {
-        this.generateProductPanels();
-
-        return <div>{this.prodList}</div>
+        return <div>{this.generateProductPanels()}</div>
     }
 }
