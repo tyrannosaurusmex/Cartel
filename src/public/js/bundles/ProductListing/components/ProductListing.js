5293110f9036eda06078ba850d8b4547f84758ef
Chris Daniells <chrisdaniells@hotmail.co.uk>
2017-01-27 17:03:02 +0000

    git push stuff now plz

--- src/public/js/bundles/ProductListing/components/ProductListing.js
+++ src/public/js/bundles/ProductListing/components/ProductListing.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Connect } from 'react-redux';
 import store from '../../../store';
+import * as Salsa from '../../../library/salsa';
+import ProductHandler from '../../../modules/ProductHandler';
 
 import ProductPanel from '../../ProductPanel/components/ProductPanel';
 
@@ -13,10 +15,19 @@ export default class ProductListing extends React.Component {
         let prodList = [];
         for (let prod in this.props.productListing) {
             if (this.props.productListing.hasOwnProperty(prod)) {
-                // Do maths from ProductPanel here instead
-
-                let product = JSON.stringify(this.props.productListing[prod]);
-                prodList.push(<ProductPanel selectedCurrency={this.props.selectedCurrency} productData={product} />)
+                let thisProduct = this.props.productListing[prod],
+                    productHandler = new ProductHandler(thisProduct),
+                    displayPrice = productHandler.getLowestVariantPrice(this.props.selectedCurrency),
+                    productPriceSymbol = productHandler.getSymbolByCurrency(this.props.selectedCurrency),
+                    fromPrice = '';
+                if (Salsa.objectLength(thisProduct.variants) > 1) {
+                    let fromPrice = <span className="ProductPanel-fromPrice">from </span>
+                }
+                prodList.push(<ProductPanel brandName={thisProduct.brand_name}
+                                            productTitle={thisProduct.product_title}
+                                            fromPrice={fromPrice}
+                                            productPrice={displayPrice}
+                                            productPriceSymbol={productPriceSymbol} />)
             }
         }
 
--- src/public/js/bundles/ProductPanel/components/ProductPanel.js
+++ src/public/js/bundles/ProductPanel/components/ProductPanel.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import * as Salsa from '../../../library/salsa';
-import ProductHandler from '../../../modules/ProductHandler';
 
 export default class ProductPanel extends React.Component {
     constructor(data) {
         super(data);
         this.props = data;
-        this.product = JSON.parse(this.props.productData);
-        this.productHandler = new ProductHandler(this.product);
-        this.displayPrice = this.productHandler.getLowestVariantPrice(this.props.selectedCurrency);
-        if (Salsa.objectLength(this.product.variants) > 1) {
-            this.from = <span className="ProductPanel-fromPrice">from </span>
-        } else {
-            this.from = '';
-        }
     }
     render() {
         <div className="ProductPanel">
             <img src="" className="productPanel-productImage" />
-            <p className="ProductPanel-productTitle">{this.product.productTitle}</p>
-            <p className="ProductPanel-productPrice">{this.from}{this.productHandler.getSymbolByCurrency(this.props.selectedCurrency)}{this.displayPrice}</p>
+            <p className="ProductPanel-productTitle">{this.props.productTitle}</p>
+            <p className="ProductPanel-productPrice">{this.props.fromPrice}{this.props.productPriceSymbol}{this.props.productPrice}</p>
             <button className="Button">View</button>
         </div>
     }
