400f2125927a144fa386d2e003f58fafce82825d
Chris Daniells <chrisdaniells@hotmail.co.uk>
2017-02-03 10:06:43 +0000

    shiz@

--- src/public/js/bundles/ProductPanel/components/ProductPanel.js
+++ src/public/js/bundles/ProductPanel/components/ProductPanel.js
@@ -11,7 +11,7 @@ export default class ProductPanel extends React.Component {
     }
     render() {
         return <div className="ProductPanel">
-            <img src="http://zumic.com/wp-content/uploads/2015/06/eagles-of-death-metal-zipper-down-album-cover-art-600x600.jpg" className="ProductPanel-productImage" />
+            <img src={this.props.productImage} className="ProductPanel-productImage" />
             <p className="ProductPanel-productTitle">{this.props.brandName} - {this.props.productTitle}</p>
             <p className="ProductPanel-productPrice">{this.fromPrice}{this.props.productPriceSymbol}{this.props.productPrice}</p>
             <button className="ProductPanel-button Button">View</button>
